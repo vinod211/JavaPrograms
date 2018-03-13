9b343ace2e4e03f3d7604e2ae46c23cc1c21784f
admin <admin@vijay>
2018-03-13 12:21:55 +0530

    sample github commit

---
+++ GitHubUpload/src/gitHubUpload/GitHubUploadClass.java
@@ -0,0 +1,10 @@
+package gitHubUpload;
+
+public class GitHubUploadClass {
+
+	public static void main(String[] args) {
+		System.out.println("this is sample of github file updation");
+
+	}
+
+}
--- MyJavaProject1/src/myfristJavaProjectPackage1/SwitchCase.java
+++ MyJavaProject1/src/myfristJavaProjectPackage1/SwitchCase.java
@@ -1,19 +1,19 @@
 package myfristJavaProjectPackage1;
 
 public class SwitchCase {
-	int b = 2;
+	
 
 	public static void main(String[] args) {
-		int b = 3, v = 5, result;
+		int b = 3, v = 5 ,result;
 
-		int excute = 1;
+	int excute = 0;
 
-		switch (excute)
-		{
-		case 1:
+		switch (excute) {
+		
+		case 0:
 			result = b + v;
 			System.out.println(result);
-
+		
 		case 2:
 			result = b - v;
 			System.out.println(result);
@@ -21,7 +21,7 @@ public class SwitchCase {
 		case 3:
 			result=b*v;
 			System.out.println(result);
-			break;
+		
 		}
 	}
 }
